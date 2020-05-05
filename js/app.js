512c2f9f13730234242a15ec4d434b3c201d573c
BelenZiegemann <beluziegemann@gmail.com>
2020-05-05 09:16:37 -0300

    Agregando estilos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
+/*
+Proyecto perteneciente a la materia Ingenieria de 
+de Aplicaciones Web. Universidad Nacional del Sur. 
+*/ 
+
+let puntuacion = 0; // 25 aciertas -15 fallas
+let numIntentos = 6;
+let numIntentosTotal= numIntentos;
+let palabraAdivinar = [];
+let palabraMostrar = [];
+let teclasBloqueadas = [];
+let pista = document.querySelector('#pista');
+let palabra = document.querySelector('#palabra')
+let intentos = document.querySelector('#intentos');
+let intentosOriginales= document.querySelector('#intentosOriginales');
+let puntuacion = document.querySelector('#puntuacionH2');
+let botonReiniciar = document.querySelector('#BotonReiniciar');
+
+function iniciarPartida(){
+  intentos.textContent = numIntentos;
+  intentosOriginales.textContent= 10;
+  pista.textContent= "hola";
+}
+
+/*
+function actualizarPantalla()
+function obtenerTecla()
+function chequearTecla()
+function estadoPartida()
+function bloquearTodasTeclas()
+function reiniciarPartida()
+*/
+
+iniciarPartida();
+
 
---
+++ js/palabras.js
@@ -0,0 +1,16 @@
+const listaPistas=[
+    'Saga de películas',
+    'Saga de Películas',
+    'Saga de Películas',
+];
+
+
+const listaPalabras=[
+    'star wars',
+    'El Señor de Los Anillos',      
+    'Transformers' 
+];
+
+
+  
+ 
